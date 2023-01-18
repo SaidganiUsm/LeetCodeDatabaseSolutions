0eeaea41b75812c82867683ad25896fcda7623d1
Saidgani Usmonov <121709409+SaidganiUsm@users.noreply.github.com>
2023-01-18 18:09:44 +0500

    Update LeetCode_Database_Solutions.sql

--- LeetCode_Database_Solutions.sql
+++ LeetCode_Database_Solutions.sql
@@ -30,3 +30,8 @@ GROUP BY Email HAVING COUNT(*) > 1
 # Write your MySQL query statement below
 SELECT player_id, MIN(event_date) AS first_login FROM Activity
 GROUP BY player_id
+
+# 586. Customer Placing the Largest Number of Orders
+# Write your MySQL query statement below
+SELECT customer_number FROM Orders
+GROUP BY customer_number ORDER BY COUNT(*) DESC LIMIT 1
